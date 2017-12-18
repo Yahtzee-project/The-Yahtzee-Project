df24160bc5ee665f61cce9b5460b032214adf4bb
Jonathan Geller <jonathangeller@comcast.net>
2017-12-18 09:26:43 -0500

    Fixed a couple boolean operations

--- Yatzee.py
+++ Yatzee.py
@@ -16,93 +16,93 @@ class scoreboard:
 
     def aces(self, dice):
     	points = 0
-    	if dice[0] = 1:
+    	if dice[0] == 1:
     		points = points+1
-    	if dice[1] = 1:
+    	if dice[1] == 1:
     		points = points+1
-    	if dice[2] = 1:
+    	if dice[2] == 1:
     		points = points+1
-		if dice[3] = 1:
+		if dice[3] == 1:
     		points = points+1
-    	if dice[4] = 1:
+    	if dice[4] == 1:
     		points = points+1
 
     	self.aces = points
 
     def twos():
     	points = 0
-    	if dice[0] = 2:
+    	if dice[0] == 2:
     		points = points+2
-    	if dice[1] = 2:
+    	if dice[1] == 2:
     		points = points+2
-    	if dice[2] = 2:
+    	if dice[2] == 2:
     		points = points+2
-		if dice[3] = 2:
+		if dice[3] == 2:
     		points = points+2
-    	if dice[4] = 2:
+    	if dice[4] == 2:
     		points = points+2
 
-    	self.aces = points
+    	self.twos = points
 
     def threes():
     	points = 0
-    	if dice[0] = 3:
+    	if dice[0] == 3:
     		points = points+3
-    	if dice[1] = 3:
+    	if dice[1] == 3:
     		points = points+3
-    	if dice[2] = 3:
+    	if dice[2] == 3:
     		points = points+3
-		if dice[3] = 3:
+		if dice[3] == 3:
     		points = points+3
-    	if dice[4] = 3:
+    	if dice[4] == 3:
     		points = points+3
 
-    	self.aces = points
+    	self.threes = points
 
     def fours():
 		points = 0
-    	if dice[0] = 4:
+    	if dice[0] == 4:
     		points = points+4
-    	if dice[1] = 4:
+    	if dice[1] == 4:
     		points = points+4
-    	if dice[2] = 4:
+    	if dice[2] == 4:
     		points = points+4
-		if dice[3] = 4:
+		if dice[3] == 4:
     		points = points+4
-    	if dice[4] = 4:
+    	if dice[4] == 4:
     		points = points+4
 
-    	self.aces = points
+    	self.fours = points
 
 	def fives():
 		points = 0
-    	if dice[0] = 5:
+    	if dice[0] == 5:
     		points = points+5
-    	if dice[1] = 5:
+    	if dice[1] == 5:
     		points = points+5
-    	if dice[2] = 5:
+    	if dice[2] == 5:
     		points = points+5
-		if dice[3] = 5:
+		if dice[3] == 5:
     		points = points+5
-    	if dice[4] = 5:
+    	if dice[4] == 5:
     		points = points+5
 
-    	self.aces = points
+    	self.fives = points
 
 	def sixes():
 		points = 0
-    	if dice[0] = 6:
+    	if dice[0] == 6:
     		points = points+6
-    	if dice[1] = 6:
+    	if dice[1] == 6:
     		points = points+6
-    	if dice[2] = 6:
+    	if dice[2] == 6:
     		points = points+6
-		if dice[3] = 6:
+		if dice[3] == 6:
     		points = points+6
-    	if dice[4] = 6:
+    	if dice[4] == 6:
     		points = points+6
 
-    	self.aces = points
+    	self.sixes = points
 
 	def three_of_a_kind(): #check
 		points = 0
@@ -122,26 +122,26 @@ class scoreboard:
 
 		self.full_house = points
 
-	def small_strait(): #check
+	def small_straight(): #check
 		points = 0
 		points = points + 30
 
-		self.full_house = points
+		self.small_straight = points
 
 	def large_strait(): #check
 		points = 0
 		points = points + 40
 
-		self.full_house = points
+		self.large_straight = points
 
 	def YAHTZEE(): #check
 		points = 0
 		points = points + 50
 
-		self.full_house = points
+		self.YAHTZEE = points
 
 	def CHANCE():
 		points = 0
 		points = int(dice[0] + dice[1] + dice[2] + dice[3] + dice[4] + dice[5])
 
-		self.four_of_a_kind = points
+		self.CHANCE = points
